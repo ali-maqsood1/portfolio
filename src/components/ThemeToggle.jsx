1eb7390544ee3fd7b9500e2c1d005cc7e793b60e
Ali Maqsood <alimaqsood35892@gmail.com>
2025-08-06 11:52:04 +0500

    Updated ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -30,12 +30,18 @@ export const ThemeToggle = () => {
         }
     }
 
-    return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-2 right-3 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outline-hidden"
-    )}>
-        {isDarkMode ? 
-            <Sun className="h-6 w-6 text-yellow-300 cursor-pointer"/> :
-            <Moon className="h-6 w-6 text-blue-900 cursor-pointer"/>
-        }
+    return (
+    <button
+        onClick={toggleTheme}
+        className={cn(
+        "top-4 right-3 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-none"
+        )}
+    >
+        {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300 cursor-pointer" />
+        ) : (
+        <Moon className="h-6 w-6 text-blue-900 cursor-pointer" />
+        )}
     </button>
 }
